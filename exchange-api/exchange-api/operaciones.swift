45aff7cabdfa0bc446eb15bcb0ed678e99c452b0
Kevin <kevin.medina@anahuac.mx>
2022-02-22 18:51:31 -0600

    Exchange API

---
+++ exchange-api/exchange-api/operaciones.swift
@@ -0,0 +1,62 @@
+//
+//  operaciones.swift
+//  exchange-api
+//
+//  Created by dansa on 22/02/22.
+//
+
+import Foundation
+// MARK: - Convertidor
+struct Convertidor: Codable {
+    let motd: MOTD
+    let success: Bool
+    let query: Query
+    let info: Info
+    let historical: Bool
+    let date: String
+    let result: Double
+}
+
+// MARK: - Info
+struct Info: Codable {
+    let rate: Double
+}
+
+// MARK: - MOTD
+struct MOTD: Codable {
+    let msg: String
+    let url: String
+}
+
+// MARK: - Query
+struct Query: Codable {
+    let from: String
+    let to: String
+    let amount: Double
+}
+
+extension Convertidor{
+    static let empty = Convertidor(motd: MOTD(msg: "", url: ""), success: false, query: Query(from: "", to: "", amount: -1.0), info: Info(rate: -1), historical: false, date: "", result: -1)
+}
+
+enum ConvertidorError: Error{
+    case invalidURL
+    case missingData
+}
+
+class APICall{
+    func vacio() async throws -> Convertidor {
+        return  Convertidor.empty
+    }
+    func getCurrency(de:String, a:String, monto:Double) async throws -> Convertidor{
+        var urlConsumo = "https://api.exchangerate.host/convert?from=USD&to=EUR"
+        urlConsumo = urlConsumo + "?from=" + de + "&to=" + a + "&amount=" + String(format: "%.1f", monto)
+        print(urlConsumo)
+        guard let url = URL(string: urlConsumo) else{
+            throw ConvertidorError.invalidURL
+        }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let conv = try JSONDecoder().decode(Convertidor.self, from:data)
+        return conv
+    }
+}
